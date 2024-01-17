76889c6e2964bbb693634499e517e12ebda56c20
Seree Rakwong <meo.rakwong@hotmail.com>
2024-01-17 10:10:30 +0700

    Create queue.c

---
+++ queue/queue.c
@@ -0,0 +1,119 @@
+/*
+File name: queue.c
+Author: Seree Meo Rakwong
+Date: 17-JAN-2024
+Purpose: Implement a queue
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "queue.h"
+
+struct queue_node_s
+{
+    struct queue_node_s* next;
+    void*   data;
+};
+
+struct queue_s
+{
+    struct queue_node_s* first;
+    struct queue_node_s* last;
+    int nitems;
+};
+
+
+queue_t* queue_new()
+{
+    queue_t* queue = (struct queue_s*)calloc(1, sizeof(struct queue_s));
+    if (!queue)
+    {
+        return 0;
+    }
+    return queue;
+}
+
+void queue_del(queue_t* queue)
+{
+    if (queue)
+    {
+        queue_deque_all(queue);
+        free(queue);
+    }
+}
+
+void queue_deque_all(queue_t* queue)
+{
+    struct queue_node_s* first = queue->first;
+    while (first)
+    {
+        queue_deque(queue);
+        first = queue->first;
+    }
+    queue->first  = queue->last = 0;
+    queue->nitems = 0;
+}
+
+void queue_deque(queue_t* queue)
+{
+    struct queue_node_s* first = queue->first;
+    if (first)
+    {
+        /*going the next*/
+        queue->first = queue->first->next;
+        --queue->nitems;
+        if (0 == queue->nitems)
+        {
+            queue->first  = queue->last = 0;
+        }
+        /*free*/
+        first->next = 0;
+        free(first->data);
+        first->data = 0;
+        free(first);
+        first = 0;
+    }
+}
+
+struct queue_node_s* queue__new_node(void* data, int sz1)
+{
+    struct queue_node_s* newnode = (struct queue_node_s*)calloc(1, sizeof(struct queue_node_s));
+    if (!newnode)
+    {
+        return 0;
+    }
+    if (data && sz1 > 0)
+    {
+        newnode->data = calloc(1, sz1);
+        if (newnode->data)
+        {
+            memcpy(newnode->data, data, sz1);
+        }
+    }
+    return newnode;
+}
+
+void* queue_enque(queue_t* queue, void* data, int sz1)
+{
+    struct queue_node_s* newnode = queue__new_node(data, sz1);
+    if (!newnode)
+    {
+        return 0;
+    }
+    if (!queue->first)
+    {
+        queue->first = queue->last = newnode;
+    }
+    else
+    {
+        queue->last->next = newnode;
+        queue->last = newnode;
+    }
+    ++queue->nitems;
+    return newnode->data;
+}
+
+void* queue_front(queue_t* queue)
+{
+    return (queue->first ? queue->first->data : 0);
+}
